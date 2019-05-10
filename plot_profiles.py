af7be28f8072195f197a33e405c0a6be19cbf6b5
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2019-05-10 09:06:03 +0200

    initial commit

---
+++ plot_profiles.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+"""
+read 2d Langmuir measurement and plot profile of floating potential
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+#datapath="/run/media/jonathan/Stick16G/Messdaten_2D_Langmuir/"
+
+analysisParameters = {}
+analysisParameters["datapath"]="/home/jonathan/Uni/Forschung/01_Messdaten/Messdaten_2D_Langmuir_Juli2018/"
+analysisParameters["outDir"] = "/home/jonathan/Uni/Forschung/03_Auswertung/2D_Langmuir_Juli2018/"
+analysisParameters["cmap"] = "jet"
+analysisParameters["contourLevels"] = 20
+analysisParameters["year"]="2018"
+analysisParameters["monthday"]="0716"
+analysisParameters["series"]="002"
+analysisParameters["magFieldLabel"] = "500mT"
+
+
+listOfAnalysisParameters = []
+listOfAnalysisParameters.append(analysisParameters)
+
+def getOutputFilePrefix(analysisParameters):
+    year          = analysisParameters["year"]
+    monthday      = analysisParameters["monthday"]
+    series        = analysisParameters["series"]
+    magFieldLabel = analysisParameters["magFieldLabel"]
+    my_cmap       = analysisParameters["cmap"]
+    contourLevels = analysisParameters["contourLevels"]
+    return year+"_"+monthday+"_"+series+"_"+magFieldLabel+"_"+my_cmap+"_"+str(contourLevels)
+
+def runAnalysis(listOfAnalysisParameters):
+    positions_subdir="2D_Control_Piezo_Linearencoder"
+    probechar_subdir="Langmuir_Kennlinie_SM2400/"
+    
+    for analysisParameters in listOfAnalysisParameters:
+        
+        datapath      = analysisParameters["datapath"]
+        year          = analysisParameters["year"]
+        monthday      = analysisParameters["monthday"]
+        series        = analysisParameters["series"]
+        magFieldLabel = analysisParameters["magFieldLabel"]
+        outDir        = analysisParameters["outDir"]
+        my_cmap       = analysisParameters["cmap"]
+        contourLevels = analysisParameters["contourLevels"]
+        outputPrefix  = getOutputFilePrefix(analysisParameters)
+        
+        
+        datadir=datapath+"/"+year+"/"+monthday+"/"+series+"/"
+        
+        print("looking for data in "+datadir)
+        print("       => saving to "+outDir+"/"+outputPrefix+"*")
+        
+        # read positions
+        posfilePiezo=datadir+"/"+positions_subdir+"/path.dat"
+        posPiezo = np.loadtxt(posfilePiezo)
+        
+        xOff = 28.5-30.0 # r
+        xScale = 0.9
+        
+        yOff = 49 # z
+        yScale = -0.9
+        
+        def scale(xLinEnc, off, scale):
+            return np.add(np.multiply(xLinEnc, scale), off)
+        
+        realX = scale(posPiezo[:,0], xOff, xScale)
+        realY = scale(posPiezo[:,1], yOff, yScale)
+        
+        num_r = 23
+        num_z = 12
+        
+        gridX = realX.reshape([num_z,num_r])
+        gridY = realY.reshape([num_z,num_r])
+        
+#        dx = np.mean(np.diff(gridX, axis=1))
+#        dy = np.mean(np.diff(gridY, axis=0))
+#        
+#        # compute corners for pcolormesh
+#        extX = np.hstack([gridX-dx/2, np.transpose([gridX[:,-1]+dx/2])])
+#        cornersX = np.vstack([extX, [extX[-1,:]]])
+#        
+#        extY=np.vstack([gridY-dy/2, [gridY[-1,:]+dy/2]])
+#        cornersY=np.hstack([extY, np.transpose([extY[:,-1]])])
+        
+        
+        
+        #plt.plot(positions[:,0], positions[:,1])
+        
+        # read vacuum probe characteristics (no plasma)
+        vacuum_meas_filename = datapath+"/"+year+"/0716/500mT_no_plasma.dat"
+        vacuum_char = np.loadtxt(vacuum_meas_filename)
+
+        # read probe characteristics
+        phif = np.zeros([num_z, num_r])
+        iisat = np.zeros([num_z, num_r])
+        i=0
+        chars=[]
+        for z in range(num_z):
+            for r in range(num_r):    
+                
+                char_filename=datadir+"/"+probechar_subdir+("/%04d-00.dat"%i)
+                #print(char_filename)
+                char_data=np.loadtxt(char_filename)
+                
+                char_data[:,1] -= vacuum_char[:,1]
+                
+                # add 99.6V to voltage (HP 6516A power supply in series to SourceMeter)
+        #        char_data[:,0]+=99.6
+                
+                chars.append(char_data)
+                phif_here=np.interp(0,char_data[:,1],char_data[:,0])
+                phif[z,r]=phif_here
+                
+                #iisat_here=np.interp(phif_here-30.0, char_data[:,0], char_data[:,1])
+                iisat_here=np.interp(0.0, char_data[:,0], char_data[:,1])
+                iisat[z,r]=iisat_here
+                
+                i+=1
+                
+        #%%
+           
+        
+        
+        
+#        def onclick(event):
+#        #    print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+#        #          ('double' if event.dblclick else 'single', event.button,
+#        #           event.x, event.y, event.xdata, event.ydata))
+#            r_idx=np.int(np.round(event.xdata)/2.0)
+#            z_idx=np.int((gridY[0,0]-np.round(event.ydata-1.0))/2.0)
+#            #print(r_idx)
+#            #print(z_idx)
+#            print("r=%g z=%g"%(gridX[z_idx,r_idx],gridY[z_idx,r_idx]))
+#            char_idx=z_idx*num_r+r_idx
+#            
+#            fig42 = plt.figure(42)
+#            plt.plot(chars[char_idx][:,0], chars[char_idx][:,1]*1e6, label="r=%g z=%g"%(gridX[z_idx,r_idx],gridY[z_idx,r_idx]))
+#        #    plt.plot(chars[char_idx][:,0], chars[char_idx][:,2], label="dI/dU")
+#            plt.xlabel("U / V")
+#            plt.ylabel("I / uA")
+#            plt.legend(loc="best")
+#            plt.grid(True)
+#            plt.tight_layout()
+#            plt.show()
+            
+        
+        #fig=plt.figure()     
+        #plt.imshow(np.flipud(phif[z0:,:]), interpolation="nearest", extent=[r_meshgrid[z0,0]-1, r_meshgrid[z0,-1]+1, z_meshgrid[z0,0]+1, z_meshgrid[-1,0]-1])
+        #plt.contourf(np.flipud(phif[z0:,:]), interpolation="nearest", extent=[gridX[z0,0]-1, gridX[z0,-1]+1, gridY[z0,0]+1, gridY[-1,0]-1], levels=20, cmap=my_cmap)
+        #plt.pcolormesh(cornersX, cornersY, phif, cmap=my_cmap)
+        plt.figure()
+        plt.contourf(gridX, gridY, phif, cmap=plt.get_cmap(my_cmap), levels=contourLevels)
+        cb=plt.colorbar()
+        cb.set_label("floating potential / V")
+        plt.xlabel("r / mm")
+        plt.ylabel("z / mm")
+        plt.title(magFieldLabel)
+        plt.tight_layout()
+        plt.savefig(outDir+"/"+outputPrefix+"_PhiF.png")
+#        cid = fig.canvas.mpl_connect('button_press_event', onclick)
+        
+        #fig2=plt.figure()     
+        #plt.imshow(np.flipud(iisat[z0:,:]*-1e6), interpolation="nearest", extent=[r_meshgrid[z0,0], r_meshgrid[z0,-1], z_meshgrid[z0,0], z_meshgrid[-1,0]])
+        #plt.contourf(np.flipud(iisat[z0:,:]*-1e6), interpolation="nearest", extent=[gridX[z0,0]-1, gridX[z0,-1]+1, gridY[z0,0]+1, gridY[-1,0]-1], levels=20, cmp=my_cmap)
+        plt.figure()
+        plt.contourf(gridX, gridY, iisat*-1e6, cmap=plt.get_cmap(my_cmap), levels=contourLevels)
+        cb=plt.colorbar()
+        cb.set_label("ion saturation current (at 30V below phif) / uA")
+        plt.xlabel("r / mm")
+        plt.ylabel("z / mm")
+        plt.title(magFieldLabel)
+        plt.tight_layout()
+        plt.savefig(outDir+"/"+outputPrefix+"_Iisat.png")
+#        cid = fig2.canvas.mpl_connect('button_press_event', onclick)
+
+
+runAnalysis(listOfAnalysisParameters)
---
+++ position_calibration.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 12:59:47 2019
+
+@author: Jonathan Schilling (jonathan.schilling@ipp.mpg.de)
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# positions from img_to_coord
+posfile = "/home/jonathan/Uni/Forschung/01_Messdaten/Messdaten_2D_Langmuir_Juli2018/2018/0716/005/ximea_QMH_main/positions.dat"
+
+# read positions from manual identification
+allImg = []
+allX   = []
+allY   = []
+with open(posfile, "r") as f:
+    for l in f.readlines():
+        
+        lineparts = l.strip().split(" ")
+        imgname = lineparts[0]
+        xPos = np.double(lineparts[1])
+        yPos = np.double(lineparts[2])
+
+        allImg.append(imgname)
+        allX.append(xPos)
+        allY.append(yPos)
+
+# read positions from piezo positioning unit
+posfilePiezo = "/home/jonathan/Uni/Forschung/01_Messdaten/Messdaten_2D_Langmuir_Juli2018/2018/0716/005/2D_Control_Piezo_Linearencoder/path.dat"
+posPiezo = np.loadtxt(posfilePiezo)
+
+def scale(xLinEnc, off, scale):
+    return np.add(np.multiply(xLinEnc, scale), off)
+
+#%%
+plt.figure()
+plt.plot(posPiezo[:,0], allX, '+')
+plt.plot(posPiezo[:,0], scale(posPiezo[:,0], 28.5, 0.9), '+')
+plt.title("X scaling")
+plt.xlabel("pos from linear encoders")
+plt.ylabel("pos from image")
+
+#%%
+plt.figure()
+plt.title("Y scaling")
+plt.plot(posPiezo[:,1], allY, '+')
+plt.plot(posPiezo[:,1], scale(posPiezo[:,1], 49, -0.9), '+')
+plt.xlabel("pos from linear encoders")
+plt.ylabel("pos from image")
+
+#%%
+
+xOff = 28.5-30.0 # r
+xScale = 0.9
+
+yOff = 49 # z
+yScale = -0.9
+
+realX = scale(posPiezo[:,0], xOff, xScale)
+realY = scale(posPiezo[:,1], yOff, yScale)
+
+plt.figure()
+plt.plot(realX, realY, 'rx', label="centers")
+
+
+gridX = realX.reshape([12,23])
+gridY = realY.reshape([12,23])
+
+dx = np.mean(np.diff(gridX, axis=1))
+dy = np.mean(np.diff(gridY, axis=0))
+
+# compute corners for pcolormesh
+extX = np.hstack([gridX-dx/2, np.transpose([gridX[:,-1]+dx/2])])
+cornersX = np.vstack([extX, [extX[-1,:]]])
+
+extY=np.vstack([gridY-dy/2, [gridY[-1,:]+dy/2]])
+cornersY=np.hstack([extY, np.transpose([extY[:,-1]])])
+
+plt.plot(cornersX, cornersY, 'b+', label="corners")
+
+plt.xlabel("r / mm")
+plt.ylabel("z / mm")
+plt.title("measured locations")
+
+
+
+
